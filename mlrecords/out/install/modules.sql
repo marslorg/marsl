39956b0f7156ff6d2abbcc906935aff852b45b9d
Marcel Linke <Marcel.Linke@gmail.com>
2012-11-30 17:50:56 +0100

    Renamed folder mlrecords to marsl

--- mlrecords/out/install/modules.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `module`(`name`,`file`,`class`) VALUES('Navigation','navigation','Navigation');
-INSERT INTO `module`(`name`,`file`,`class`) VALUES('URLLoader','urlloader','URLLoader');
-INSERT INTO `module`(`name`,`file`,`class`) VALUES('Galerie','gallery','Gallery');
-INSERT INTO `module`(`name`,`file`,`class`) VALUES('Nachrichten','news','News');
-INSERT INTO `module`(`name`,`file`,`class`) VALUES('Portal','portal','Portal');
-INSERT INTO `module`(`name`,`file`,`class`) VALUES('Benutzerverwaltung','userdata','UserData');
-INSERT INTO `module`(`name`,`file`,`class`) VALUES('Forum','board','Board');
--- mlrecords/out/template/portal.foot.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php
-include_once (dirname(__FILE__)."/../includes/errorHandler.php");
-?>
