18a8b72ce649bc753bab7be8d2ecf0b66800f0db
Marcel Linke <Marcel.Linke@gmail.com>
2012-12-02 19:04:33 +0100

    Reverted plupload to the last running version.

--- marsl/out/admin/template/gallery.upload.tpl.php
+++ marsl/out/admin/template/gallery.upload.tpl.php
@@ -6,7 +6,7 @@ include_once (dirname(__FILE__)."/../../includes/errorHandler.php");
 $(function() {
 	$("#uploader").pluploadQueue({
 		// General settings
-		runtimes : 'silverlight,flash,html5',
+		runtimes : 'silverlight,gears,flash,browserplus,html5',
 		url : 'galleryuploader.php?dir=<?php echo $tmpDir; ?>',
 		max_file_size : '30mb',
 		chunk_size : '1mb',
@@ -17,31 +17,14 @@ $(function() {
 
 		// Specify what files to browse for
 		filters : [
-			{title : "Image files", extensions : "jpg,gif,png"}
+			{title : "Image files", extensions : "jpg,gif,png"},
 		],
 
 		// Flash settings
-		flash_swf_url : '../includes/jscripts/plupload/js/Moxie.swf',
+		flash_swf_url : '../includes/jscripts/plupload/js/plupload.flash.swf',
 
 		// Silverlight settings
-		silverlight_xap_url : '../includes/jscripts/plupload/js/Moxie.xap',
-
-		// Rename files by clicking on their titles
-		rename: true,
-		
-		// Sort files
-		sortable: true,
-
-		// Enable ability to drop files onto the widget (currently only HTML5 supports that)
-		dragdrop: true,
-
-		// Views to activate
-		views: {
-			list: true,
-			thumbs: true // Show thumbs
-		},
-		default_view: 'thumbs',
-		remember_view: true // requires jquery cookie plugin
+		silverlight_xap_url : '../includes/jscripts/plupload/js/plupload.silverlight.xap'
 	});
 
 	// Client side form validation
@@ -70,7 +53,7 @@ $(function() {
 <div class="success">Die Galerie wurde erfolgreich eingestellt!</div>
 <?php endif; ?>
 <div id="uploader">
-	Dein Browser unterst&uuml;tzt kein Silverlight, Flash oder HTML5.
+	Dein Browser unterst&uuml;tzt kein Flash, Silverlight, Gears, BrowserPlus oder HTML5.
 </div>
 
 <form method="post" action="index.php?var=module&amp;module=gallery&amp;step=2&amp;dir=<?php echo $tmpDir; ?>">
