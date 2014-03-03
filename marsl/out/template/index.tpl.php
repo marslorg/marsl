0d524abd2ff5879acc5e9a9abf4fcc1de2f9e93a
Marcel Linke <Marcel.Linke@gmail.com>
2014-03-03 22:20:53 +0100

    Changed Ads.

--- marsl/out/template/index.tpl.php
+++ marsl/out/template/index.tpl.php
@@ -58,6 +58,8 @@ include_once (dirname(__FILE__)."/../includes/errorHandler.php");
 			<div class="fb-like-box" data-href="http://www.facebook.com/music2web" data-width="260" data-show-faces="true" data-stream="false" data-header="true"></div>
 			<div class="right_ads">
 				Anzeigen:<br />
+				<a href="http://www.dressfirst.es/Vestidos-De-Novia-c2/" target="_blank">vestidos de novia</a><br />
+				<a href="http://www.amormoda.nl/Avondjurken-c17/">avondjurken</a><br />
 				<a href="http://www.jennyjoseph.de/Kleider-Fur-Besondere-Anlasse-c3/" target="_blank">abendkleider</a><br />
 				<a href="http://standesamtverzeichnis.de/" target="_blank">Standesamt</a><br />
 				<?php if (((!isset($_GET['id']))&&(!isset($_GET['scope'])))||(isset($_GET['id'])&&($_GET['id']=="178"))): ?>
