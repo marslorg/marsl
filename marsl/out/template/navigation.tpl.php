8bf2435b21130845e5a0400c1ee1baebd5ee0244
linke <Marcel.Linke@gmail.com>
2021-03-18 18:54:20 +0100

    Performance optimizations

--- marsl/out/template/navigation.tpl.php
+++ marsl/out/template/navigation.tpl.php
@@ -1,23 +1,26 @@
 <?php 
 include_once (dirname(__FILE__)."/../includes/errorHandler.php");
 ?>
+<?php foreach($categories as $category): ?>
 <div class="box">
 	<div class="box-title">
-		<?php if ($cat_type == 1): ?><a href="index.php?id=<?php echo $cat_id; ?>"><?php endif; ?>
-			<?php echo $cat_name; ?>
-		<?php if ($cat_type == 1): ?></a><?php endif; ?>
+		<?php if ($category['type'] == 1): ?><a href="index.php?id=<?php echo $category['id']; ?>"><?php endif; ?>
+			<?php echo $category['name']; ?>
+		<?php if ($category['type'] == 1): ?></a><?php endif; ?>
 	</div>
 
 	<div class="box-content">
 		<ul class="nice-list">
-			<?php foreach ($links as $link): ?>
+			<?php if ($category['type'] == 0 && array_key_exists($category['id'], $links)): ?>
+			<?php foreach ($links[$category['id']] as $link): ?>
 			<li>
 				<a href="index.php?id=<?php echo $link['id']; ?>">
 					<?php echo $link['name']; ?>
 				</a>
 			</li>
 			<?php endforeach; ?>
-									
+			<?php endif; ?>									
 		</ul>
 	</div>
 </div>
+<?php endforeach; ?>
