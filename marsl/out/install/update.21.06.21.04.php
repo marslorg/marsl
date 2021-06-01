4d44949dc8537a2ca55899172caf83b879d1ae21
linke <Marcel.Linke@gmail.com>
2021-06-01 19:43:47 +0200

    Introduced expo push token and further db indexes

---
+++ marsl/out/install/update.21.06.21.04.php
@@ -0,0 +1,25 @@
+<?php
+include_once(dirname(__FILE__)."/../includes/errorHandler.php");
+include_once(dirname(__FILE__)."/../includes/dbsocket.php");
+include_once(dirname(__FILE__)."/../includes/config.inc.php");
+
+class Install {
+	public function startInstall() {
+		$config = new Configuration();
+		date_default_timezone_set($config->getTimezone());
+		$db = new DB();
+		$db->connect();
+		$content = file_get_contents("update.21.06.21.04.sql");
+		$statement = strtok($content, ";");
+		while ($statement) {
+			$db->query($statement);
+            $statement = strtok(";");	
+            echo $statement."<br />";
+		}
+		$db->close();
+	}
+}
+
+$install = new Install();
+$install->startInstall();
+?>
