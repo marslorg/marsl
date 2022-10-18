cd59166a42cb05659d898d826018802e73683a15
linke <Marcel.Linke@gmail.com>
2022-10-18 18:18:44 +0200

    Dockerfile has to run under root for Kubernetes

--- marsl/out/startup/index.php
+++ marsl/out/startup/index.php
@@ -1,9 +1,25 @@
 <?php
+include_once(dirname(__FILE__)."/../includes/dbsocket.php");
+
 class Main {
 
     public function display() {
-        http_response_code(200);
-        echo "HEALTHY";
+        $db = new DB();
+
+        $healthy = $db->isHealthy();
+        $healthy = $healthy && file_exists(dirname(__FILE__)."/../albums/health");
+        $healthy = $healthy && file_exists(dirname(__FILE__)."/../files/health");
+        $healthy = $healthy && file_exists(dirname(__FILE__)."/../news/health");
+        $healthy = $healthy && file_exists(dirname(__FILE__)."/../shared/health");
+
+        if ($healthy) {
+            http_response_code(200);
+            echo "HEALTHY";
+        }
+        else {
+            http_response_code(500);
+            echo "UNHEALTHY";
+        }
 	}
 
 }
