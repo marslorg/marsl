731bafe4ef491cb0febd30ba2b810355cbeb816f
linke <Marcel.Linke@gmail.com>
2022-11-20 23:33:09 +0100

    Introduction of a responsive design

--- marsl/out/template/index.tpl.php
+++ marsl/out/template/index.tpl.php
@@ -7,6 +7,7 @@ include_once (dirname(__FILE__)."/../includes/errorHandler.php");
 	<head>
 		<title><?php echo $title; ?></title>
 		<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta property="fb:app_id" content="<?php echo $fbcomments; ?>" />
 		<?php if ($image!=null): ?>
 		<meta property="og:image" content="<?php echo $domain; ?>/<?php echo $image; ?>" />
@@ -16,13 +17,14 @@ include_once (dirname(__FILE__)."/../includes/errorHandler.php");
 		<script type="text/javascript" src="includes/jscripts/jquery/jquery-ui.js"></script>
 		<script type="text/javascript" src="includes/jscripts/photoswipe/photoswipe.min.js"></script>
 		<script type="text/javascript" src="includes/jscripts/photoswipe/photoswipe-ui-default.min.js"></script>
-		<script type="text/javascript" src="http://bp.yahooapis.com/2.4.21/browserplus-min.js"></script>
 		<script type="text/javascript" src="includes/jscripts/plupload/js/plupload.full.min.js"></script>
 		<script type="text/javascript" src="includes/jscripts/plupload/js/jquery.plupload.queue/jquery.plupload.queue.js"></script>
 		<script type="text/javascript" src="includes/jscripts/plupload/js/i18n/de.js"></script>
 		<link rel="alternate" type="application/rss+xml" title="<?php echo $title; ?> - RSS Feed" href="<?php echo $domain; ?>/rss.php" />
-		<link rel="stylesheet" href="styles/style.css" type="text/css" />
-		<link rel="stylesheet" href="styles/portal.css" type="text/css" />
+		<link rel="stylesheet" href="styles/style.css?v=1" type="text/css" />
+		<link rel="stylesheet" href="styles/menu.css?v=1" type="text/css" />
+		<link rel="stylesheet" href="styles/portal.css?v=1" type="text/css" />
+		<link rel="stylesheet" href="styles/mobile.css?v=1" type="text/css" media="all and (max-width: 1023px)" />
 		<link rel="stylesheet" href="includes/jscripts/plupload/js/jquery.plupload.queue/css/jquery.plupload.queue.css" type="text/css" />
 		<link rel="stylesheet" href="includes/jscripts/photoswipe/photoswipe.css" type="text/css" />
 		<link rel="stylesheet" href="includes/jscripts/photoswipe/default-skin/default-skin.css" type="text/css" />
@@ -39,7 +41,7 @@ include_once (dirname(__FILE__)."/../includes/errorHandler.php");
 
 				<div id="topnav">
 					<div class="moduletab_menu">
-						<ul class="menu">
+						<ul class="links">
 							<li><a href="<?php echo $domain; ?>/rss.php" target="_blank">RSS Feed</a></li>
 							<li><a href="index.php?id=100">Disclaimer</a>
 							<li><a href="index.php?id=74">Impressum</a></li>
@@ -50,8 +52,13 @@ include_once (dirname(__FILE__)."/../includes/errorHandler.php");
 					<?php $this->displaySearchBox(); ?>
 				</div>
 			</div>
-			<div id="topmenu">
-				<ul class="menu">
+			<input type="checkbox" id="menu_responsive" />
+			<label for="menu_responsive" class="menu_responsive_label">
+				<span>&#9776;</span>
+				Navigation
+			</label>
+			<div class="menu">
+				<ul>
 					<?php $navigation->display(); ?>
 				</ul>
 			</div>
