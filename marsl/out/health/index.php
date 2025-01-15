5a7e8776b8d2bc77a14cba9ee5daf5521b449dbd
marcellinke <Marcel.Linke@gmail.com>
2025-01-15 20:06:55 +0100

    Added health check diagnostic

--- marsl/out/health/index.php
+++ marsl/out/health/index.php
@@ -1,10 +1,58 @@
 <?php
+include_once(dirname(__FILE__)."/../includes/config.inc.php");
+include_once(dirname(__FILE__)."/../includes/basic.php");
+include_once(dirname(__FILE__)."/../includes/dbsocket.php");
+
 class Main {
 
     public function display() {
+        $result = "";
+        if (isset($_GET['diagnostic']) && $_GET['diagnostic'] == "true") {
+            header("Cache-Control: no-cache, must-revalidate");
+            header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+            header("Access-Control-Allow-Origin: *");
+            header("Content-Type: application/json; charset=UTF-8");
+            header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+            header("Access-Control-Max-Age: 3600");
+            header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+            $config = new Configuration();
+            $db = new DB();
+            $db->connect();
+            $role = new Role($db);
+		    $auth = new Authentication($db, $role);
+            $basic = new Basic($db, $auth, $role);
+
+            $resultArray['serverName'] = $basic->convertToHTMLEntities($config->getClusterServer());
+            $resultArray['databaseTime'] = $this->getTimeForDatabase($db);
+            $resultArray['albumsFolderTime'] = $this->getTimeForFolder("albums");
+            $resultArray['filesFolderTime'] = $this->getTimeForFolder("files");
+            $resultArray['newsFolderTime'] = $this->getTimeForFolder("news");
+            $resultArray['sharedFolderTime'] = $this->getTimeForFolder("shared");
+
+            $jsonMessage = json_encode($resultArray);
+            $result = $jsonMessage;
+        }
+        else {
+            $result = "HEALTHY";
+        }
         http_response_code(200);
-        echo "HEALTHY";
+        echo $result;
 	}
+
+    private function getTimeForDatabase($db) {
+        $startTimeForDatabase = microtime(true);
+        $db->isHealthy();
+        $stopTimeForDatabase = microtime(true);
+        return $stopTimeForDatabase - $startTimeForDatabase;
+    }
+
+    private function getTimeForFolder($folderName) {
+        $startTimeForDatabase = microtime(true);
+        file_exists(dirname(__FILE__)."/../".$folderName."/health");
+        $stopTimeForDatabase = microtime(true);
+        return $stopTimeForDatabase - $startTimeForDatabase;
+    }
 
 }
 
