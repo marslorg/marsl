f04014d5b176057af1d10e7678ee972b29603fc1
marcellinke <Marcel.Linke@gmail.com>
2025-05-06 23:42:11 +0200

    Fixed case-sensitiveness in autoloader

--- marsl/out/autoload.php
+++ marsl/out/autoload.php
@@ -2,16 +2,26 @@
 
 spl_autoload_register("psr4Autoloader");
 
-function psr4Autoloader(string $classPath) : void {
+function psr4Autoloader(string $classPath): void
+{
     $relativeFilePath = str_replace("marsl", "", str_replace("\\", "/", $classPath));
 
     $absoluteFilePath = dirname(__FILE__).$relativeFilePath.".php";
 
     if (!file_exists($absoluteFilePath)) {
-        $absoluteFilePath = dirname(__FILE__).strtolower($relativeFilePath).".php";
+        $lowerFilePath = strtolower($absoluteFilePath);
+
+        $filePathes = glob(dirname($absoluteFilePath) . '/*');
+
+        if (is_array($filePathes)) {
+            foreach ($filePathes as $filePath) {
+                if (strtolower($filePath) == $lowerFilePath) {
+                    $absoluteFilePath = $filePath;
+                    break;
+                }
+            }
+        }
     }
 
     include_once($absoluteFilePath);
 }
-
-?>
